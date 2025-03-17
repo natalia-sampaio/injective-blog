f24333c97c0b55fd3624417f02257406534bc9de
Natalia Andrade <natalia.srsa@proton.me>
2025-03-17 12:50:22 -0300

    update workflow for studio deploy

--- studio-layer/sanity.cli.ts
+++ studio-layer/sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'l2fdudnh',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'injective-blog',
 })
